b22d2c4886c1e0e8efd6f790213d5b361cbf5616
Simao Novais <snovaisg.97@gmail.com>
2019-11-29 22:11:41 +0000

    Create create_taxis_table.sql

---
+++ create_taxis_table.sql
@@ -0,0 +1,25 @@
+CREATE EXTERNAL TABLE IF NOT EXISTS taxis
+(
+row_idx int,
+id string,
+vendor_id int,
+pickup_datetime TIMESTAMP,
+dropoff_datetime TIMESTAMP,
+passager_count int,
+pickup_longitude float,
+pickup_latitude float,
+dropoff_longitude float,
+dropoff_latitude float,
+haversine_distance float,
+maximum_temperature int,
+minimum_temperature int,
+average_temperature float,
+precipitation float,
+snow_fall float,
+snow_depth float
+)
+
+
+row format delimited fields terminated by ',';
+
+load data local inpath '/root/work/taxis.csv' overwrite into table taxis;
