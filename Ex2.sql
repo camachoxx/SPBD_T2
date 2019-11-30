62fb07c2b320114cfb9fdc6f4b66ceefa5a5e491
João Camacho <j.camacho@campus.fct.unl.pt>
2019-11-30 03:02:29 +0000

    Create Ex2.sql

---
+++ Ex2.sql
@@ -0,0 +1,9 @@
+
+CREATE VIEW IF NOT EXISTS T2 as
+SELECT from_unixtime(unix_timestamp(pickup_datetime), 'EEEE') as w,
+pickoff_datetime,dropoff_datetime,haversine_distance,
+CONCAT(ROUND(pickup_latitude,2),", ",ROUND(pickup_longitude,2)) as pickup_area
+FROM taxis;
+
+--A final SELECT for the last operations
+
