ec74880917bf3a0d7aae62cb617740d4d414cbc9
Simao Novais <snovaisg.97@gmail.com>
2019-12-06 15:13:07 +0000

    Added necessary field for ex1 and ex2 in create_taxis_table.sql to remove redundancy

--- create_taxis_table.sql
+++ create_taxis_table.sql
@@ -24,13 +24,15 @@ row format delimited fields terminated by ',';
 
 load data local inpath '/root/work/taxis.csv' overwrite into table taxis;
 
-CREATE VIEW IF NOT EXISTS taxis_preprocessed as
+DROP TABLE taxis_preprocessed;
+CREATE TABLE taxis_preprocessed as
 select row_idx,id vendor_id, pickup_datetime, dropoff_datetime, passager_count,
 pickup_longitude, pickup_latitude,
 CONCAT(ROUND(pickup_latitude,2),", ",ROUND(pickup_longitude,2)) as pickup_area,
 CONCAT(ROUND(dropoff_latitude,2),", ",ROUND(dropoff_longitude,2)) as dropoff_area,
+from_unixtime(unix_timestamp(pickup_datetime), 'EEE') as week_day_,
 dropoff_longitude, dropoff_latitude, haversine_distance, maximum_temperature,
 minimum_temperature, average_temperature, precipitation, snow_fall, snow_depth
 from taxis;
 
-select * from taxis_preprocessed limit 9;
+select * from taxis_preprocessed limit 3;
