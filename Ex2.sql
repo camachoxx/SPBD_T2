309e5fe7b905252d4caa833f5216a0134a40cd20
Simao Novais <snovaisg.97@gmail.com>
2019-12-06 15:12:22 +0000

    Improved variable names in Ex2.sql

--- Ex2.sql
+++ Ex2.sql
@@ -1,17 +1,20 @@
-Drop view if exists T2_temp1;
-CREATE VIEW IF NOT EXISTS T2_temp1 as
+Drop view if exists time_preprocessed;
+CREATE VIEW IF NOT EXISTS time_preprocessed as
 select haversine_distance, pickup_datetime, dropoff_datetime,
 from_unixtime(unix_timestamp(date_format(pickup_datetime, 'y-M-d H:m:00')) - minute(pickup_datetime)*60 + (INT(minute(pickup_datetime) / 15)* 15*60 ),'H:m:00') as time_,
 pickup_area,
-from_unixtime(unix_timestamp(pickup_datetime), 'EEE') as week_day_name_
+from_unixtime(unix_timestamp(pickup_datetime), 'EEE') as week_day_name_,
+(unix_timestamp(dropoff_datetime) - unix_timestamp(pickup_datetime))/60 as duration_minutes
 from taxis_preprocessed;
 
+Drop Table if exists T2_answer;
+CREATE Table T2_answer as
 select week_day_name_,time_,pickup_area,
-AVG(unix_timestamp(pickup_datetime)-unix_timestamp(dropoff_datetime)) as avg_duration,
-AVG(haversine_distance) as avg_distance
-from T2_temp1
+INT(AVG(duration_minutes)) as avg_duration_minutes,
+INT(AVG(haversine_distance)) as avg_distance
+from time_preprocessed
 group by pickup_area,week_day_name_,time_
-limit 10;
+order by pickup_area,week_day_name_,time_ desc;
 
 
 --select avg(haversine_distance) from T2
