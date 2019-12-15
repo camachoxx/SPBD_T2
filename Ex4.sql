ea02bbf7588bac4c1c8ca515a1e2c38688ee0deb
Simao Novais <snovaisg.97@gmail.com>
2019-12-15 23:47:42 +0000

    Update Ex4.sql
    Ready to deliver Ex4

--- Ex4.sql
+++ Ex4.sql
@@ -1,16 +1,33 @@
-DROP VIEW IF EXISTS taxis_preprocessed;
-
-CREATE VIEW IF NOT EXISTS T4 as
-SELECT (dropoff_datetime - pickup_datetime) as travel_time,
-CONCAT(CONCAT(ROUND(pickup_latitude,1),", ",ROUND(pickup_longitude,1)),", ",
-CONCAT(ROUND(dropoff_latitude,1),", ",ROUND(dropoff_longitude,1))) as r,
+-- get necessary info from the dataset
+DROP VIEW IF EXISTS vendor_routes_and_travel_time;
+CREATE VIEW vendor_routes_and_travel_time as
+SELECT unix_timestamp(dropoff_datetime) - unix_timestamp(pickup_datetime) as travel_time,
+CONCAT(pickup_area,", ",dropoff_area) as route,
 vendor_id
-FROM taxis;
+FROM taxis_preprocessed;
 
-SELECT *
-FROM T4
-DISTRIBUTE BY r;
+-- get average travel time for each route and vendor
 
-DROP VIEW IF EXISTS T4;
-DROP VIEW IF EXISTS V0;
-DROP VIEW IF EXISTS V1;
+DROP VIEW IF EXISTS avg_travel_time_per_route_per_vendor;
+CREATE VIEW avg_travel_time_per_route_per_vendor as
+SELECT vendor_id, route, avg(travel_time) as avg_travel_time
+FROM vendor_routes_and_travel_time
+group by route,vendor_id;
+
+--seperate both vendors
+
+DROP VIEW IF EXISTS vendor_1;
+CREATE VIEW vendor_1 as
+select * from avg_travel_time_per_route_per_vendor
+where vendor_id = 0;
+
+DROP VIEW IF EXISTS vendor_2;
+CREATE VIEW vendor_2 as
+select * from avg_travel_time_per_route_per_vendor
+where vendor_id = 1;
+
+-- compute final index that allows to compare avg travel time of both vendors on each route
+Drop Table if exists T4_answer;
+CREATE Table T4_answer as
+SELECT vendor_1.route, (vendor_1.avg_travel_time / 60) as vendor_1_avg_travel_time_minutes, (vendor_2.avg_travel_time / 60) as vendor_2_avg_travel_time_minutes
+from vendor_1 inner join vendor_2 on vendor_1.route = vendor_2.route;
